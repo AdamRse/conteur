583f8e20b13c169a90cec8d3333479c4c132340a
AdamRse <bakaarion3@gmail.com>
2026-01-11 18:01:14 +0100

    set test

--- test.sh
+++ test.sh
@@ -19,20 +19,22 @@ ex() {
 # Tests de fonctions
 debug_ "DEBUG MODE ON"
 
-zal="ok zal"
-var_name="zala"
-if [ -n "${!var_name}" ]; then
-    export $var_name
-    echo "Variable \$$var_name expotée"
-else
-    wout "La variable '$var_name' passée en 3ème paramètre de 'copy_file_from_template()' ne pointe sur aucune valeur, elle est ignoré et ne modifiera pas le template. Vérifiez le nom de la variable passée à 'copy_file_from_template()', elle doit comporter une erreur de nom."
-fi
+conf_reader
 
-if declare -p zal 2>/dev/null | grep -q "^declare -x"; then
-    echo "$var_name est exportée, contenu : $zal"
-else
-    echo "$var_name n'est pas exportée ou n'existe pas"
-fi
+# zal="ok zal"
+# var_name="zala"
+# if [ -n "${!var_name}" ]; then
+#     export $var_name
+#     echo "Variable \$$var_name expotée"
+# else
+#     wout "La variable '$var_name' passée en 3ème paramètre de 'copy_file_from_template()' ne pointe sur aucune valeur, elle est ignoré et ne modifiera pas le template. Vérifiez le nom de la variable passée à 'copy_file_from_template()', elle doit comporter une erreur de nom."
+# fi
+
+# if declare -p zal 2>/dev/null | grep -q "^declare -x"; then
+#     echo "$var_name est exportée, contenu : $zal"
+# else
+#     echo "$var_name n'est pas exportée ou n'existe pas"
+# fi
 
 # lout "lout"
 # sout "sout"
