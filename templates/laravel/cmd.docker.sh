32da2ea0cb3e08ae665ce674f773b206c003d11e
AdamRse <adam.rousselle@proton.me>
2026-01-28 15:07:53 +0100

    changement de cmd.docker.sh, il va créé un faux projet pour test

--- templates/laravel/cmd.docker.sh
+++ templates/laravel/cmd.docker.sh
@@ -1,10 +1,15 @@
 # -- VARIABLES GLOBALES DISPONIBLES --
 
-# ${LARAVEL_VERSION} : Version de laravel:latest (Exemple "12.1.1.0")
-# ${PHP_VERSION} : Version de PHP requise pour laravel:latest (Exemple "8.4")
-# ${PROJECTS_DIR} : Emplacement local où créer le répertoire du projet (Exemple "/home/user/projets")
-# ${PROJECT_PATH} : Emplacement local du répertoire du projet qui contient les fichiers du projet (Exemple "/home/user/projets/nom_projet")
+# ${LARAVEL_VERSION}    : Version de laravel:latest (Exemple "12.1.1.0")
+# ${PHP_VERSION}        : Version de PHP requise pour laravel:latest (Exemple "8.4")
+
+# ${PROJECT_NAME}       : Nom du projet, PATH friendly
+# ${PROJECTS_DIR}       : Emplacement local où créer le répertoire du projet (Exemple "/home/user/projets")
+# ${PROJECT_PATH}       : Emplacement local du répertoire du projet qui contient les fichiers du projet (Exemple "/home/user/projets/nom_projet")
 
 # --
 
 # -- COMMANDE(S) DOCKER A EFFECTUER
+
+mkdir -p "${PROJECT_PATH}"
+echo "<h1>Nouveau Projet ${PROJECT_NAME}</h1><p>Page de test pour le projet ${PROJECT_PATH}</p>" > "${PROJECT_PATH}/index.html"
